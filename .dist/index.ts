d9fe366dfe9cdbf481d9fbdd286201a5e9a8e53e
Ethimuz <Ethimuz@gmail.com>
2020-10-21 18:02:30 -0300

    primer commit

---
+++ .dist/index.ts
@@ -0,0 +1,168 @@
+function getNombre(codigo : string) {
+    
+    switch (codigo) {
+        case 'Sy' :
+            document!.getElementById("cientifica")!.innerHTML = "Maria Silbylla Merian";
+            break;
+        case 'Av' :
+            document!.getElementById("cientifica")!.innerHTML = "Ángeles Alvariño";
+            break;
+        case 'Ba' :
+            document!.getElementById("cientifica")!.innerHTML = "Florence Bascom";
+            break;
+        case 'Ca' :
+            document!.getElementById("cientifica")!.innerHTML = "Rachel Carson";
+            break;
+        case 'Cv' :
+            document!.getElementById("cientifica")!.innerHTML = "Josefina Castellvi";
+            break;
+        case 'Lh' :
+            document!.getElementById("cientifica")!.innerHTML = "Inge Lehman";
+            break;
+        case 'Mh' :
+            document!.getElementById("cientifica")!.innerHTML = "Wangari Maathai";
+            break;
+        case 'St' :
+            document!.getElementById("cientifica")!.innerHTML = "Marie Stopes";
+            break;
+        case 'Th' :
+            document!.getElementById("cientifica")!.innerHTML = "Marie Tharp y Sylvia Earle";
+            break;
+        case 'Ay' :
+            document!.getElementById("cientifica")!.innerHTML = "Hertha Ayrton";
+            break;
+        case 'Cb' :
+            document!.getElementById("cientifica")!.innerHTML = "Pilar Carbonero";
+            break;
+        case 'Cr' :
+            document!.getElementById("cientifica")!.innerHTML = "Josephine Cochrane";
+            break;
+        case 'Ha' :
+            document!.getElementById("cientifica")!.innerHTML = "Margaret Hamilton";
+            break; 
+        case 'Ho':
+            document!.getElementById("cientifica")!.innerHTML = "Grace Hopper";
+            break;
+        case 'K':
+            document!.getElementById("cientifica")!.innerHTML = "Stephanie Kwolek";
+            break;                  
+        case 'La':
+            document!.getElementById("cientifica")!.innerHTML = "Hedy Lamarr";
+            break;
+        case 'Rr':
+            document!.getElementById("cientifica")!.innerHTML = "Ángela Ruiz Robles";
+            break;    
+        case 'Ag':
+            document!.getElementById("cientifica")!.innerHTML = "Maria Agnessi";
+            break;     
+        case 'Bs':
+            document!.getElementById("cientifica")!.innerHTML = "Laura Bassi";
+            break;
+        case 'By':
+            document!.getElementById("cientifica")!.innerHTML = "Pilar Bayer";
+            break;    
+        case 'C':
+            document!.getElementById("cientifica")!.innerHTML = "Emilie du Chátelet";
+            break; 
+        case 'Cl':
+            document!.getElementById("cientifica")!.innerHTML = "M. Antónia Canals";        
+            break;
+        case 'Cs':
+            document!.getElementById("cientifica")!.innerHTML = "M. Andrea Casamayor";       
+            break;
+        case 'Ge':
+            document!.getElementById("cientifica")!.innerHTML = "Sophie Germain";       
+            break;
+        case 'Gv':
+            document!.getElementById("cientifica")!.innerHTML = "Evelyn Boyd Granville";       
+            break;
+        case 'H':
+            document!.getElementById("cientifica")!.innerHTML = "Hipatia de Alejandria";       
+            break;
+        case 'Jh':
+            document!.getElementById("cientifica")!.innerHTML = "Katherine Johnson";       
+            break;
+        case 'Ko':
+            document!.getElementById("cientifica")!.innerHTML = "Sofia Kovalevskaya";       
+            break;
+        case 'Lo':
+            document!.getElementById("cientifica")!.innerHTML = "Ada Lovelace";       
+            break;
+        case 'Lp':
+            document!.getElementById("cientifica")!.innerHTML = "Nicole-Reine de Lepaute";       
+            break;
+        case 'Mz':
+            document!.getElementById("cientifica")!.innerHTML = "Maryam Mirzajani";       
+            break;
+        case 'No':
+            document!.getElementById("cientifica")!.innerHTML = "Emmy Noether";       
+            break;
+        case 'Sm':
+            document!.getElementById("cientifica")!.innerHTML = "Mary Sommerville";       
+            break;
+        case 'Wo':
+            document!.getElementById("cientifica")!.innerHTML = "Maria Wonenburger";       
+            break;
+        case 'An':
+            document!.getElementById("cientifica")!.innerHTML = "Mary Anning";       
+            break;
+        case 'Lk':
+            document!.getElementById("cientifica")!.innerHTML = "Mary Leakey";       
+            break;
+        case 'Ma':
+            document!.getElementById("cientifica")!.innerHTML = "Maria Martinón Torres";       
+            break;
+        case 'Al':
+            document!.getElementById("cientifica")!.innerHTML = "Frances H. Amold";       
+            break;
+        case 'Bb':
+            document!.getElementById("cientifica")!.innerHTML = "Katharine Burr Blodgett";       
+            break;                            
+        case 'Bn':
+            document!.getElementById("cientifica")!.innerHTML = "Dorotea Barnés y las químicas españolas de la edad de plata";       
+            break;
+        case 'Cw':
+            document!.getElementById("cientifica")!.innerHTML = "Dorothy Crowfoot Hodgkin";       
+            break; 
+        case 'Fr':
+            document!.getElementById("cientifica")!.innerHTML = "Rosalind Franklin";       
+            break;
+        case 'Fu':
+            document!.getElementById("cientifica")!.innerHTML = "Gertrudis de la Fuente";       
+            break;
+        case 'Ju':
+            document!.getElementById("cientifica")!.innerHTML = "Manuela Juárez";       
+            break;
+        case 'Ln':
+            document!.getElementById("cientifica")!.innerHTML = "Kathleen Lonsdale";       
+            break;                            
+        case 'Me':
+            document!.getElementById("cientifica")!.innerHTML = "Marie Meurdrac & Jane Marcet";       
+            break;
+        case 'Mn':
+            document!.getElementById("cientifica")!.innerHTML = "Rosa M. Menédez";       
+            break; 
+        case 'Nd':
+            document!.getElementById("cientifica")!.innerHTML = "Ida Nodaack";       
+            break;
+        case 'Pc':
+            document!.getElementById("cientifica")!.innerHTML = "Agnes Pockels";       
+            break;
+        case 'Pz':
+            document!.getElementById("cientifica")!.innerHTML = "Marie Anne Paulze";       
+            break;
+        case 'Rc':
+            document!.getElementById("cientifica")!.innerHTML = "Eilen Richards";       
+            break;
+        case 'Yn':
+            document!.getElementById("cientifica")!.innerHTML = "Ada Yonath";       
+            break;
+        case 'Gd':    
+            document.getElementById("cientifica")!.innerHTML = "Jane Goodall, Dian Fossey & Biruté Galdikas";
+            break;
+        default :
+            break;                 
+                     
+    } 
+    
+}
